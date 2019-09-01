3c9d47fbae75b6f764c7891045c0800e443e6e5b
Matthew Gleich <43759105+Matt-Gleich@users.noreply.github.com>
2019-09-01 17:27:53 -0400

    Initial commit

---
+++ src/tests/unittest_example.py
@@ -0,0 +1,13 @@
+import unittest
+import sys
+
+sys.path.append("..")
+
+class TestFILENAME(unittest.TestCase):
+    """
+    Tests for the file FILENAME
+    """
+
+
+if __name__ == "__main__":
+    unittest.main()
